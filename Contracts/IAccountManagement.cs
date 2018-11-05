d863368190b15ead62d50f33415fea25b5d2d1b0
Nemanja Simic <snemanja.simic@gmail.com>
2018-11-05 17:24:54 +0100

    Initial commit

---
+++ Contracts/IAccountManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Contracts
+{
+   [ServiceContract]
+    public interface IAccountManagement
+    {
+        [OperationContract]
+        bool CreateAccount();
+        [OperationContract]
+        bool DeleteAccount();
+        [OperationContract]
+        bool ResetPassword();
+    }
+}
---
+++ Contracts/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Contracts
+{
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public String Username { get; set; }
+        [DataMember]
+        public SecureString Password {get; set;}
+    }
+}
