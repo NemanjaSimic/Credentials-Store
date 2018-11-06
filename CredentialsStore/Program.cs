0304034dba7f3e33ca004a0f4ffbcef2b4166480
dusannosovic <39407532+dusannosovic@users.noreply.github.com>
2018-11-06 14:45:21 +0100

    CredentialHostDone

--- CredentialsStore/Program.cs
+++ CredentialsStore/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using Contracts;
+using System.ServiceModel.Description;
 
 namespace CredentialsStore
 {
@@ -10,7 +13,28 @@ namespace CredentialsStore
     {
         static void Main(string[] args)
         {
+            NetTcpBinding binding = new NetTcpBinding();
+            binding.Security.Mode = SecurityMode.Transport;
 
+            binding.Security.Transport.ProtectionLevel = System.Net.Security.ProtectionLevel.EncryptAndSign;
+
+            binding.Security.Transport.ClientCredentialType = TcpClientCredentialType.Windows;
+
+            string address = "net.tcp://localhost:9999/AccountManagement";
+
+            ServiceHost host = new ServiceHost(typeof(AccountManagement));
+            host.AddServiceEndpoint(typeof(IAccountManagement), binding, address);
+
+            host.Description.Behaviors.Remove(typeof(ServiceDebugBehavior));
+            host.Description.Behaviors.Add(new ServiceDebugBehavior() { IncludeExceptionDetailInFaults = true });
+
+
+            host.Open();
+            Console.WriteLine("WCFService is opened. Press <enter> to finish...");
+
+            Console.ReadLine();
+
+            host.Close();
         }
     }
 }
