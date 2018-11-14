f4fbb2d8070166a32699183a97a3a257b5065370
dusannosovic <39407532+dusannosovic@users.noreply.github.com>
2018-11-14 12:04:20 +0100

    Password validation now working

--- Security.cs/PasswordPolicy.cs
+++ Security.cs/PasswordPolicy.cs
@@ -57,31 +57,70 @@ namespace Security
         public static bool ValidatePassword(string password)
         {
 			LoadRules();
-			bool retVal = true;
+			bool retVal = false;
 
 			if (complexPass)
 			{
-				if (!string.IsNullOrEmpty(password.ToString()))
-				{
-					if (password.Length < 7)
-						retVal = false;
-					if (Regex.Match(password, @"/\d+/", RegexOptions.ECMAScript).Success)
-						retVal = false;
-					if (Regex.Match(password, @"/[a-z]/", RegexOptions.ECMAScript).Success &&
-					  Regex.Match(password, @"/[A-Z]/", RegexOptions.ECMAScript).Success)
-						retVal = false;
-					if (Regex.Match(password, @"/.[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]/", RegexOptions.ECMAScript).Success)
-						retVal = false;
-				}
-				//string patternPassword = @"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$";
-				//if (!string.IsNullOrEmpty(password))
-				//{
-				//	if (Regex.IsMatch(password, patternPassword))
-				//	{
-				//		retVal = false;
-				//	}
-				//}
-			}
+                //if (!string.IsNullOrEmpty(password.ToString()))
+                //{
+                //	if (password.Length < 7)
+                //		retVal = false;
+                //                if (!Regex.Match(password, @"/\d+/", RegexOptions.ECMAScript).Success)
+                //                    retVal = false;
+                //                if (!Regex.Match(password, @"/[a-z]/", RegexOptions.ECMAScript).Success &&
+                //                  !Regex.Match(password, @"/[A-Z]/", RegexOptions.ECMAScript).Success)
+                //                    retVal = false;
+                //                if (!Regex.Match(password, @"/.[!,@,#,$,%,^,&,*,?,_,~,-,£,(,)]/", RegexOptions.ECMAScript).Success)
+                //		retVal = false;
+                //}
+                //string patternPassword = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$";
+                //if (!string.IsNullOrEmpty(password))
+                //{
+                //    if (Regex.IsMatch(password, patternPassword))
+                //    {
+                //        retVal = false;
+                //    }
+                //}
+                var input = password;
+
+
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    return false;
+                }
+
+                var hasNumber = new Regex(@"[0-9]+");
+                var hasUpperChar = new Regex(@"[A-Z]+");
+                var hasMiniMaxChars = new Regex(@".{8,15}");
+                var hasLowerChar = new Regex(@"[a-z]+");
+                var hasSymbols = new Regex(@"[!@#$%^&*()_+=\[{\]};:<>|./?,-]");
+
+                if (!hasLowerChar.IsMatch(input))
+                {
+                    return false;
+                }
+                else if (!hasUpperChar.IsMatch(input))
+                {
+                    return false;
+                }
+                else if (!hasMiniMaxChars.IsMatch(input))
+                {
+                    return false;
+                }
+                else if (!hasNumber.IsMatch(input))
+                {
+                    return false;
+                }
+
+                else if (!hasSymbols.IsMatch(input))
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
 
             return retVal;
         }
